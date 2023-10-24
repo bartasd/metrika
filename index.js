1b2cdaae981d105c49639e3e4218da991d89b746
Bartas Dausynas <bartas3@gmail.com>
2023-10-25 00:07:06 +0300

    nebdirba smegenai

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const drop_menu = document.getElementById('down');
-let droped = false;
+let dropedM = false;
 
 drop_menu.addEventListener('click', function onClick() {
-    droped = !droped;
-    if(droped)
+    dropedM = !dropedM;
+    if(dropedM)
         drop_menu.classList.add('flipH');
     else
         drop_menu.classList.remove('flipH');
+});
+
+const drop_panel = document.getElementById('down_panel');
+let dropedP = false;
+
+drop_panel.addEventListener('click', function onClick() {
+    dropedP = !dropedP;
+    if(dropedP)
+        drop_panel.classList.add('flipH');
+    else
+        drop_panel.classList.remove('flipH');
 });
