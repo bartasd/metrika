c9b63378783975a7178eecd77621026f9895300c
Bartas Dausynas <bartas3@gmail.com>
2023-11-07 21:31:41 +0200

    1st js generated html eva

---
+++ js/genMenuItems.js
@@ -0,0 +1,51 @@
+export class Menu {
+    constructor(selector, rows, icons) {
+        this.selector = selector;
+        this.rows = rows;
+        this.icons = icons;
+        this.len = null;
+        this.DOM = null;
+
+        this.init();
+    }
+
+    init() {
+        const err = this.updateDOMelement();
+        if (typeof err === 'string') {
+            return console.error(err);
+        }
+        this.len = this.testRowIconsLength();
+        if(this.len === 'string')
+            return console.error(err2);
+
+        this.render();
+    }
+
+    updateDOMelement() {
+        if (typeof this.selector !== 'string' || this.selector === '') {
+            return 'Netinkamas selektorius';
+        }
+        this.DOM = document.querySelector(this.selector);
+    }
+
+    testRowIconsLength(){
+        if(this.rows.length !== this.icons.length)
+            return "Eilutėms trūksta iconų";
+        else
+            return this.rows.length;
+    }
+
+    render() {
+        let HTML = '';
+
+        for (let i = 0; i < this.len; i++) {
+            HTML += `
+                <div>
+                    <p class="fa fa-${this.icons[i]}"></p>
+                    <p>${this.rows[i]}</p>
+                </div>`;
+        }
+
+        this.DOM.innerHTML = HTML;
+    }    
+}
--- js/index.js
+++ js/index.js
@@ -19,3 +19,11 @@ drop_panel.addEventListener('click', function onClick() {
     else
         drop_panel.classList.remove('flipH');
 });
+
+import { Menu } from "./genMenuItems.js";
+
+const rows = ["Overview", "Product", "Order", "Analitics", "Store", "Customer", "Transactions", "Campaign"];
+const fonts = ["cubes", "cube", "shopping-bag", "pie-chart", "shopping-cart", "users", "credit-card-alt", "bullhorn" ];
+
+
+const menius = new Menu('#menu', rows, fonts);
